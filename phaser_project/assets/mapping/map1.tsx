0b2df36a1d377787aaab592038cab645468b8248
Coward-Montblanc <127381991+Coward-Montblanc@users.noreply.github.com>
2025-10-23 16:27:34 +0900

    Delete phaser_project directory

--- phaser_project/assets/mapping/map1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="map1" tilewidth="16" tileheight="16" tilecount="5487" columns="59">
- <image source="../map/map1_orgin.png" width="946" height="1500"/>
-</tileset>
